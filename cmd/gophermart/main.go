e2110110b952edcc0283bf93242c9f758981dcdd
E.Sitnikov <e.sitnikov@etpgpb.ru>
2024-09-22 15:19:26 +0300

    fixes by review, day 2

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -2,52 +2,28 @@ package main
 
 import (
 	"context"
-	"github.com/fasdalf/train-go-musthave-diploma/internal/accrual"
-	"github.com/fasdalf/train-go-musthave-diploma/internal/config"
-	dbConn "github.com/fasdalf/train-go-musthave-diploma/internal/db/connection"
-	"github.com/fasdalf/train-go-musthave-diploma/internal/http/server"
+	"github.com/fasdalf/train-go-musthave-diploma/internal/app"
 	"log/slog"
 	"os"
 	"os/signal"
-	"sync"
 )
 
 func main() {
 	ctx, ctxCancel := context.WithCancel(context.Background())
 	defer ctxCancel()
-	cfg, err := config.NewConfig()
+	mainApp, err := app.NewApp(ctx)
 	if err != nil {
-		slog.Error("failed to parse config", "error", err)
+		slog.Error("failed to init app", "error", err)
 		panic(err)
 	}
-	slog.Info("init DB connection")
-	db, err := dbConn.NewConnection(ctx, cfg.DatabaseURI)
-	if err != nil {
-		slog.Error("failed to init DB", "error", err)
-		panic(err)
-	}
-	slog.Info("DB successfully migrated")
-
-	wg := &sync.WaitGroup{}
-
-	slog.Debug("initializing http router")
-	srv := server.NewServer(db, cfg.Addr, &cfg.CryptoKey, cfg.TokenExp)
-
-	server.StartServer(srv, wg)
-	accrual.StartChecker(ctx, wg, db, &cfg.Accrual)
+	mainApp.Run()
 
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, os.Interrupt)
 	<-quit
 	slog.Info("interrupt signal received")
-	signal.Stop(quit)
-	slog.Info("shutting down http server...")
-	err = srv.Shutdown(ctx)
+	err = mainApp.Shutdown(ctxCancel)
 	if err != nil {
-		slog.Error("Server shutdown error:", "error", err)
+		slog.Error("failed to init app", "error", err)
 	}
-	ctxCancel()
-	slog.Info("waiting for bg processes...")
-	// Let worker's transactions to end gracefully.
-	wg.Wait()
 }
---
+++ internal/app/main.go
@@ -0,0 +1,57 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"github.com/fasdalf/train-go-musthave-diploma/internal/accrual"
+	"github.com/fasdalf/train-go-musthave-diploma/internal/config"
+	dbConn "github.com/fasdalf/train-go-musthave-diploma/internal/db/connection"
+	"github.com/fasdalf/train-go-musthave-diploma/internal/http/server"
+	"gorm.io/gorm"
+	"net/http"
+	"sync"
+)
+
+type App struct {
+	ctx        context.Context
+	wg         *sync.WaitGroup
+	cfg        *config.Config
+	db         *gorm.DB
+	httpServer *http.Server
+}
+
+func NewApp(ctx context.Context) (*App, error) {
+	cfg, err := config.NewConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+	db, err := dbConn.NewConnection(ctx, cfg.DatabaseURI)
+	if err != nil {
+		return nil, fmt.Errorf("failed to init DB connection: %w", err)
+	}
+	srv := server.NewServer(db, cfg.Addr, &cfg.CryptoKey, cfg.TokenExp)
+
+	return &App{
+		ctx:        ctx,
+		cfg:        cfg,
+		db:         db,
+		wg:         &sync.WaitGroup{},
+		httpServer: srv,
+	}, nil
+}
+
+func (a *App) Run() {
+	server.StartServer(a.httpServer, a.wg)
+	accrual.StartChecker(a.ctx, a.wg, a.db, &a.cfg.Accrual)
+}
+
+func (a *App) Shutdown(cancel context.CancelFunc) error {
+	err := a.httpServer.Shutdown(a.ctx)
+	if err != nil {
+		return fmt.Errorf("server shutdown error: %w", err)
+	}
+	cancel()
+	// Let worker's transactions to end gracefully.
+	a.wg.Wait()
+	return nil
+}
